e7e08a1b9389addf2a9d6ce1b1c60a5901a19d6e
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2022-07-06 23:48:56 +0900

    fix

--- networking/ssh_server.py
+++ networking/ssh_server.py
@@ -1,58 +1,60 @@
-import os 
+#!/usr/bin/env python
+import os
 import paramiko
 import socket
 import sys
 import threading
 
 CWD = os.path.dirname(os.path.realpath(__file__))
-HOSTKEY =paramiko.RSAKey(filename=os.path.join(CWD, 'test_rsa.key'))
+HOSTKEY = paramiko.RSAKey(filename=os.path.join(CWD, 'test_rsa.key'))
 
-class Server(paramiko.ServerInterface):
-    def __init__(self):
+
+class Server (paramiko.ServerInterface):
+    def _init_(self):
         self.event = threading.Event()
-        
+
     def check_channel_request(self, kind, chanid):
         if kind == 'session':
             return paramiko.OPEN_SUCCEEDED
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
-      
+
     def check_auth_password(self, username, password):
-      if (username == 'tim') and (password == 'sekret'):
-          return paramiko.AUTH_SUCCESSFUL
-        
+        if (username == 'tim') and (password == 'sekret'):
+            return paramiko.AUTH_SUCCESSFUL
+
+
 if __name__ == '__main__':
-    server = '192.168.1.207' #変更要
+    server = '192.168.1.207'
     ssh_port = 2222
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.bind((server, ssh_port))
-        sock.listen(50)
+        sock.listen(100)
         print('[+] Listening for connection ...')
         client, addr = sock.accept()
     except Exception as e:
-        print('[-] Listening failed: ' + str(e))
+        print('[-] Listen failed: ' + str(e))
         sys.exit(1)
     else:
-        print(f'[+] Got a conection! from {addr}')
-        
+        print(f'[+] Got a connection! from {addr}')
+
     bhSession = paramiko.Transport(client)
     bhSession.add_server_key(HOSTKEY)
     server = Server()
     bhSession.start_server(server=server)
-    
+
     chan = bhSession.accept(20)
     if chan is None:
-        print('*** No Channel.')
-        sys.exit()
-    
+        print('*** No channel.')
+        sys.exit(1)
+
     print('[+] Authenticated!')
     print(chan.recv(1024).decode())
     chan.send('Welcome to bh_ssh')
-    
     try:
         while True:
-            command = input("Enter command:")
+            command = input("Enter command: ")
             if command != 'exit':
                 chan.send(command)
                 r = chan.recv(8192)
@@ -63,4 +65,4 @@ if __name__ == '__main__':
                 bhSession.close()
                 break
     except KeyboardInterrupt:
-      bhSession.close()
+        bhSession.close()
