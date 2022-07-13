4301a6f037efb3c685f789b6f8b15613d4d8f8d1
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2022-07-13 22:17:16 +0900

    fix

--- scapy/recapper.py
+++ scapy/recapper.py
@@ -1,6 +1,8 @@
 from scapy.all import TCP, rdpcap
 import collections
-import os, re, sys
+import os
+import re
+import sys
 import zlib
 
 OUTDIR = '/home/kali/pictures'
@@ -15,23 +17,24 @@ def get_header(payload):
         sys.stdout.write('-')
         sys.stdout.flush()
         return None
-      
+    
     try:
-        header = dict(re.findall(r'(?P<name>.*?):(?P<value>.*?)\r\n', header_raw.decode()))
+        header = dict(re.findall(r'(?P<name>.*?): (?P<value>.*?)\r\n', header_raw.decode()))
         if 'Content-Type' not in header:
             return None
     except:
         return None
-      
+
     return header
-  
+
+
 def extract_content(Response, content_name='image'):
     content, content_type = None, None
     try:
         if content_name in Response.header['Content-Type']:
             content_type = Response.header['Content-Type'].split('/')[1]
             content = Response.payload[Response.payload.index(b'\r\n\r\n')+4:]
-            
+
             if 'Content-Encoding' in Response.header:
                 if Response.header['Content-Encoding'] == "gzip":
                     content = zlib.decompress(Response.payload, zlib.MAX_WBITS | 16)
@@ -39,17 +42,16 @@ def extract_content(Response, content_name='image'):
                     content = zlib.decompress(Response.payload)
     except:
         pass
-      
+
     return content, content_type
-        
-  
-  
+
+
 class Recapper:
     def __init__(self, fname):
         pcap = rdpcap(fname)
         self.sessions = pcap.sessions()
         self.responses = list()
-        
+
     def get_responses(self):
         for session in self.sessions:
             payloads = list()
@@ -62,6 +64,7 @@ class Recapper:
                 except IndexError:
                     sys.stdout.write('x')
                     sys.stdout.flush()
+            
             for payload in payloads:
                 if payload:
                     header = get_header(payload)
@@ -69,17 +72,17 @@ class Recapper:
                         continue
                     self.responses.append(Response(header=header, payload=payload))
         print('')
-      
+
     def write(self, content_name):
         for i, response in enumerate(self.responses):
             content, content_type = extract_content(response, content_name)
             if content and content_type:
                 fname = os.path.join(OUTDIR, f'ex_{i}.{content_type}')
-                print(f'Writting {fname}')
+                print(f'Writing {fname}')
                 with open(fname, 'wb') as f:
                     f.write(content)
-                    
-      
+
+
 if __name__ == '__main__':
     pfile = os.path.join(PCAPS, 'pcap.pcap')
     recapper = Recapper(pfile)
